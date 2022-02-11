f074f15b3e7a542fcb8331f0834099a2aadf0451
AlekseyB86 <avbelov86@mail.ru>
2022-02-11 21:29:49 +0300

    finished crawler hhru & sjru

--- lesson_6/jobparser/runner.py
+++ lesson_6/jobparser/runner.py
@@ -7,16 +7,11 @@ from jobparser.spiders.hhru import HhruSpider
 from jobparser.spiders.sjru import SjruSpider
 
 
-def start_crawler(crawler, crawler_settings=None):
-    process = CrawlerProcess(settings=crawler_settings)
-    process.crawl(crawler)
-    process.start()
-
-
 if __name__ == '__main__':
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
 
-    start_crawler(HhruSpider, crawler_settings)
-
-    start_crawler(SjruSpider, crawler_settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(SjruSpider)
+    process.crawl(HhruSpider)
+    process.start()
