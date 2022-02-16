8648fccf97bfefd7fbd174ed5bbb84fc98edeb5c
AlekseyB86 <avbelov86@mail.ru>
2022-02-16 18:06:40 +0300

    lesson_8 - get items

--- lesson_8/instaparser/spiders/instagram.py
+++ lesson_8/instaparser/spiders/instagram.py
@@ -4,8 +4,10 @@ import re
 
 import scrapy
 from scrapy.http import HtmlResponse
+from instaparser.items import InstaparserItem
 from urllib.parse import urlencode
 from dotenv import load_dotenv
+from copy import deepcopy
 
 load_dotenv()
 
@@ -15,11 +17,11 @@ class InstaSpider(scrapy.Spider):
     allowed_domains = ['instagram.com']
     start_urls = ['https://www.instagram.com/']
     inst_login_link = 'https://www.instagram.com/accounts/login/ajax/'
-    inst_login = os.getenv('USERNAME')
-    inst_password = os.getenv('PASSWORD')
+    inst_login = os.getenv('INST_LOGIN')
+    inst_password = os.getenv('INST_PASSWORD')
     user_parse = 'techskills_2022'
     graphql_url = 'https://www.instagram.com/graphql/query/?'
-    post_hash = ''
+    post_hash = '8c2a529969ee035a5063f2fc8602a0fd'
 
     def parse(self, response: HtmlResponse, **kwargs):
         csrf = self.fetch_csrf_token(response.text)
@@ -50,10 +52,31 @@ class InstaSpider(scrapy.Spider):
                               callback=self.user_posts_parse,
                               cb_kwargs={'username': username,
                                          'user_id': user_id,
-                                         'variables': variables})
+                                         'variables': deepcopy(variables)})
 
     def user_posts_parse(self, response: HtmlResponse, username, user_id, variables):
-        print()
+        j_data = response.json()
+        page_info = j_data.get('data').get('user').get('edge_owner_to_timeline_media').get('page_info')
+        if page_info.get('has_next_page'):
+            variables['after'] = page_info.get('end_cursor')
+
+            url_posts = f'{self.graphql_url}query_hash={self.post_hash}&{urlencode(variables)}'
+
+            yield response.follow(url_posts,
+                                  callback=self.user_posts_parse,
+                                  cb_kwargs={'username': username,
+                                             'user_id': user_id,
+                                             'variables': deepcopy(variables)})
+
+            posts = j_data.get('data').get('user').get('edge_owner_to_timeline_media').get('edges')
+            for post in posts:
+                yield InstaparserItem(
+                    user_id=user_id,
+                    username=username,
+                    photo=post.get('node').get('display_url'),
+                    likes=post.get('node').get('edge_media_preview_like').get('count'),
+                    post_data=post.get('node')
+                )
 
     @staticmethod
     def fetch_csrf_token(text):
