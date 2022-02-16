2ea950718256038050d640734507ac0d19a16585
AlekseyB86 <avbelov86@mail.ru>
2022-02-16 16:45:02 +0300

    lesson_8 - .env - user_data_parse

--- lesson_8/instaparser/spiders/instagram.py
+++ lesson_8/instaparser/spiders/instagram.py
@@ -1,7 +1,13 @@
+import json
+import os
 import re
 
 import scrapy
 from scrapy.http import HtmlResponse
+from urllib.parse import urlencode
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class InstaSpider(scrapy.Spider):
@@ -9,11 +15,13 @@ class InstaSpider(scrapy.Spider):
     allowed_domains = ['instagram.com']
     start_urls = ['https://www.instagram.com/']
     inst_login_link = 'https://www.instagram.com/accounts/login/ajax/'
-    inst_login = 'Alekseyb86'
-    inst_password = '#PWD_INSTAGRAM_BROWSER:10:1645011472:AY5QAGDLHBY29czWEzT9dd2J6DnKWsW5+8ud1Zf5PSRdU4Vq9ey6S0gfOikY0gapUrokrqnD3W/1q9XGwsfW3InJGfsXFTHC48fie90n3mp18pVeYcMN2urU/BpMpmnWbJMlDrlNHsNM4tZ8qBM='
+    inst_login = os.getenv('USERNAME')
+    inst_password = os.getenv('PASSWORD')
     user_parse = 'techskills_2022'
+    graphql_url = 'https://www.instagram.com/graphql/query/?'
+    post_hash = ''
 
-    def parse(self, response: HtmlResponse):
+    def parse(self, response: HtmlResponse, **kwargs):
         csrf = self.fetch_csrf_token(response.text)
         yield scrapy.FormRequest(self.inst_login_link,
                                  method='POST',
@@ -32,8 +40,30 @@ class InstaSpider(scrapy.Spider):
             )
 
     def user_data_parse(self, response: HtmlResponse, username):
+        user_id = self.fetch_user_id(response.text, username)
+        variables = {'id': user_id,
+                     'first': 12}
+
+        url_posts = f'{self.graphql_url}query_hash={self.post_hash}&{urlencode(variables)}'
+
+        yield response.follow(url_posts,
+                              callback=self.user_posts_parse,
+                              cb_kwargs={'username': username,
+                                         'user_id': user_id,
+                                         'variables': variables})
+
+    def user_posts_parse(self, response: HtmlResponse, username, user_id, variables):
         print()
 
     @staticmethod
     def fetch_csrf_token(text):
-        return re.search(r'\"csrf_token\":\"(\w+)\"', text).group(1)
+        matched = re.search('\"csrf_token\":\"\\w+\"', text).group()
+        return matched.split(':').pop().replace(r'"', '')
+
+    @staticmethod
+    def fetch_user_id(text, username):
+        try:
+            matched = re.search('{\"id\":\"\\d+\",\"username\":\"%s\"}' % username, text).group()
+            return json.loads(matched).get('id')
+        except:
+            return re.findall('{\"id\":\"\\d+\"', text)[-1].split('"')[-2]
